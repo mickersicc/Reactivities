4c6c5429c2fabaa855bd6178ec35db39fc82e0bf
mickersicc <mickersicc@gmail.com>
2020-04-07 16:26:55 -0400

    Added dashboard and list components

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Menu, Container, Button } from "semantic-ui-react";
+
+export const NavBar = () => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item name="home">
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
